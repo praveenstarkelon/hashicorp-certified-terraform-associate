31f5e03cdd745709875933d21e19f78a6b2e25ed
praveenstarkelon <devopsenginnerpraveen@gmail.com>
2024-06-11 12:58:37 +0530

    mm

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -18,7 +18,7 @@ provider "aws" {
    #added after local user setup
 }
 
-# Resource Block
+######### Resource Block
 resource "aws_instance" "ec2demo" {
   ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
