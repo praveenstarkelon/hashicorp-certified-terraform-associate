fc18b865172c131d3068e513b50adaec783833b5
Praveen Kumar <praveen.kumar@excmgmt.com>
2024-06-11 11:52:10 +0530

    new-line-added-by-headless-after-swithcing-remote-then-switch-c-brnach-name,then-add-then-commit

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -12,8 +12,9 @@ terraform {
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   #region  = "eu-west-1"
-   region  = "eu-west-1"
-
+   #region  = "eu-west-1"
+   #changes by headless state
+   region = "eu-west-1"
 }
 
 # Resource Block
